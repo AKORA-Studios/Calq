ce4f7927f6ea517912cb6575b449b01b079e8a04
Kiara <frida.shivaa@gmail.com>
2022-06-29 15:55:11 +0200

    :bug: updated git link

--- Calq/Tabs/Settings/SettingsView.swift
+++ Calq/Tabs/Settings/SettingsView.swift
@@ -208,7 +208,7 @@ class SettingsView: ViewController,  UITableViewDelegate, UITableViewDataSource,
         models.append(Section(title: "Allgemein", options: [
             .staticCell(model:SettingsOption(title: "Github", subtitle: "", icon: UIImage(systemName: "info.circle.fill"), iconBackgroundColor: UIColor.init(hexString: "#63d3bd"))
                         {
-                            let url = URL(string: "https://github.com/AKORA-Studios/Calq-iOS")
+                            let url = URL(string: "https://github.com/AKORA-Studios/Calq")
                             UIApplication.shared.open(url!)
                         }),
             
