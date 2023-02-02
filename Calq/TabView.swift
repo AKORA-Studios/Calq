2244872140851071a519c69551ffde2591a85673
DragonCat - Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-02-02 22:24:41 +0100

    Update TabView.swift

--- Calq/TabView.swift
+++ Calq/TabView.swift
@@ -11,27 +11,20 @@ struct TabbarView: View {
     
     var body: some View {
         TabView {
-               OverviewScreen().tabItem{
-                    Image(systemName: "chart.bar.fill")
-               }
-
-                SubjectListScreen().tabItem{
-                   Image(systemName: "books.vertical.fill")
-               }
-
-               NewGradeScreen().tabItem{
-                    Image(systemName: "plus.app.fill")
-               }
-               
-               ExamScreen().tabItem{
-                   Image(systemName: "text.book.closed.fill")
-               }
-               
-               SettingsScreen()
-                .navigationTitle("Settings")
-                .tabItem{
-                    Image(systemName: "gearshape.fill")
-               }
-           }
+            OverviewScreen()
+                .tabItem{Image(systemName: "chart.bar.fill")}
+            
+            SubjectListScreen()
+                .tabItem{Image(systemName: "books.vertical.fill")}
+            
+            NewGradeScreen()
+                .tabItem{Image(systemName: "plus.app.fill")}
+            
+            ExamScreen()
+                .tabItem{ Image(systemName: "text.book.closed.fill")}
+            
+            SettingsScreen()
+                .tabItem{Image(systemName: "gearshape.fill")}
+        }
     }
 }
