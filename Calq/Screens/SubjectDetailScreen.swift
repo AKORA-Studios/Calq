8d5011f6d36abb9392f5f521f9bdebd033afc286
DragonCat - Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-02-05 18:48:30 +0100

    Update SubjectDetailScreen.swift

--- Calq/Screens/SubjectDetailScreen.swift
+++ Calq/Screens/SubjectDetailScreen.swift
@@ -11,13 +11,51 @@ struct SubjectDetailScreen: View {
     @Binding var subject: UserSubject?
     @State var isGradeTablePresented = false
     
+    @State var halfyearActive = false
+    @State var slectedYear = 1
+    
     var body: some View {
         NavigationView {
             if(subject !=  nil){
-            VStack{
-                Text("aloha")
-            }.navigationTitle(subject!.name)
-        }
+                VStack{
+                    Text("insert inechart here xd") //TODO: linechart, notenliste
+                    
+                    
+                    VStack{
+                        //Year picker
+                        ZStack{
+                            VStack(alignment: .leading){
+                                Text("Halbjahr")
+                                Picker("", selection: $slectedYear) {
+                                    Text("1").tag(1)
+                                    Text("2").tag(2)
+                                    Text("3").tag(3)
+                                    Text("4").tag(4)
+                                }.pickerStyle(.segmented)
+                            }.padding()
+                        }
+                        //Year toggle
+                        HStack{
+                            Toggle(isOn: $halfyearActive) {
+                                Text("Halbjahr einbringen")
+                            }
+                            
+                        } .padding()
+                        
+                        //average chart
+                        
+                        
+                    }.background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.2)))
+                        .padding()
+                    
+                    VStack(spacing: 5){
+                        Text("Durchschnitt des Halbjahres")
+                        CircleChart(perrcent: 0.5, upperText: String(format: "%.2f", 0.5)).frame(height: 150)
+                    }.background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.2))).padding()
+                    
+                    
+                }.navigationTitle(subject!.name)
+            }
         }
     }
 }
