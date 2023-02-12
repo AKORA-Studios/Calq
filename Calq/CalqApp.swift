5c221cb03ed14c9a4b77a614929770c53d31910f
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-02-12 12:00:22 -0800

    Update CalqApp.swift

--- Calq/CalqApp.swift
+++ Calq/CalqApp.swift
@@ -10,14 +10,14 @@ import SwiftUI
 @main
 struct Calq: App {
     let persistenceController = CoreDataStack.shared
-    
+   
     var body: some Scene {
         WindowGroup {
-            TabbarView()
-                .environment(\.managedObjectContext, persistenceController.workingContext)
+                TabbarView()
+                    .environment(\.managedObjectContext, persistenceController.workingContext)
+                    .onAppear{
+                        UserDefaults.standard.register(defaults: ["firstLaunch": true])
+                    }
         }
     }
 }
-
-
-
