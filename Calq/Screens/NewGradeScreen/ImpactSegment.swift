e81130362c944358e5fd8a4608c3933b94418040
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-02-15 07:38:16 -0800

    moved view extension

--- Calq/Screens/NewGradeScreen/ImpactSegment.swift
+++ Calq/Screens/NewGradeScreen/ImpactSegment.swift
@@ -155,27 +155,3 @@ func get15Values() -> [String]{
     }
     return arr
 }
-
-//Round left and right side
-struct RoundedCorner: Shape {
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 4, height: 4))
-        return Path(path.cgPath)
-    }
-}
-
-extension View {
-    func leftcorner() -> some View {
-        clipShape(RoundedCorner(corners: [.topLeft, .bottomLeft]))
-    }
-    
-    func rightCorner() -> some View {
-        clipShape(RoundedCorner(corners: [.topRight, .bottomRight]))
-    }
-    
-    func topCorner() -> some View {
-        clipShape(RoundedCorner(corners: [.topLeft, .topRight]))
-    }
-}
