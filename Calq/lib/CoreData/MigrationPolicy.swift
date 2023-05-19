f218c0ef292d6c3d80b005bc89b897234307f89b
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-05-19 13:25:13 +0200

    custom migration policy

---
+++ Calq/lib/CoreData/MigrationPolicy.swift
@@ -0,0 +1,26 @@
+//
+//  MigrationPolicy.swift
+//  Calq
+//
+//  Created by Kiara on 19.05.23.
+//
+
+import CoreData
+
+class MigrationV0toV1: NSEntityMigrationPolicy {
+    @objc func typeFor(isLK:NSNumber) -> NSNumber {
+         if isLK.boolValue {
+             return NSNumber(integerLiteral: 1)
+         } else {
+             return NSNumber(integerLiteral: 0)
+         }
+     }
+    
+    @objc func convert(isLK: Bool) -> Int16 {
+        return isLK ? 1 : 0;
+    }
+}
+
+
+//FUNCTION($entityPolicy, "typeForWithisLK:", $source.big)
+//FUNCTION($entityPolicy, "convertWithisLK:", $source.big)
