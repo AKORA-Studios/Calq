eb01bd709bb17d7d186ab73def221ff96647f08d
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-09-05 18:31:17 +0200

    move json tests

--- CalqTests/CalqTests.swift
+++ CalqTests/CalqTests.swift
@@ -76,55 +76,6 @@ final class CalqTests: XCTestCase {
         XCTAssertEqual(types, 2)
     }
     
-    // MARK: JSON Funcs
-    func testLoadDemoData() {
-        JSON.loadDemoData()
-        let count = Util.getAllSubjects().count
-        
-        XCTAssertFalse(count == 0)
-        XCTAssertEqual(count, 12)
-    }
-    
-    func testExport() {
-        let data = "idk"
-        let url =  JSON.writeJSON(data)
-        
-        do {
-            let fileContent = try String(contentsOf: url, encoding: .utf8)
-            XCTAssertEqual(data, fileContent)
-        } catch {
-            return  assertionFailure("Exported File is empty")
-        }
-    }
-    
-    func testImportV0() {
-        Util.deleteSettings()
-        
-        let testBundle = Bundle(for: type(of: self))
-        guard let ressourceURL = testBundle.url(forResource: "exampleData_v0", withExtension: "json") else {
-            return assertionFailure("ExampleFile does not exist")
-        }
-        
-        do { try JSON.importJSONfromDevice(ressourceURL) } catch { return assertionFailure("Failed to load resource") }
-        XCTAssertEqual(Util.getAllSubjects().count, 1)
-    }
-    
-    func testImportV1() {
-        Util.deleteSettings()
-        
-        let testBundle = Bundle(for: type(of: self))
-        guard let ressourceURL = testBundle.url(forResource: "exampleData_v1", withExtension: "json") else {
-            return assertionFailure("ExampleFile does not exist")
-        }
-        
-        do { try JSON.importJSONfromDevice(ressourceURL) } catch { return  assertionFailure("Failed to load resource") }
-        XCTAssertEqual(Util.getAllSubjects().count, 1)
-    }
-    
-    func testImportV2() {
-        // TODO:
-    }
-    
     func testAverageString() {
         let average = Util.averageString(MockDataProvider.getSubjectWithTests())
         XCTAssertEqual(average, "-- 11 -- -- ")
