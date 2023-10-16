86d59675d135e326be777e85468bf33ec7ea4c8f
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-10-16 10:11:02 +0200

    Update ExamWidget.swift

--- CalqWidget/ExamWidget.swift
+++ CalqWidget/ExamWidget.swift
@@ -1,4 +1,16 @@
+//
+//  ExamWidget.swift
+//  CalqWidgetExtension
+//
+//  Created by Kiara on 16.19.23.
+//
 
-
+import SwiftUI
+    
+    
 struct ExamWidget: View { 
+  var body: some View {
+    Text("h")
+  }
+}
   
