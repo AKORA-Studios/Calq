fa736174575cce9ba23a2b88639458dbf0712a58
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-10-16 10:31:51 +0200

    Update ExamWidget.swift

--- CalqWidget/ExamWidget.swift
+++ CalqWidget/ExamWidget.swift
@@ -8,9 +8,33 @@
 import SwiftUI
     
     
-struct ExamWidget: View { 
-  var body: some View {
-    Text("h")
-  }
+struct WidgetExamEntry: Identifiable {
+    var points: Int
+    var name: String
+    let id = UUID()
+}
+
+struct ExamWidget: View {
+    var exams = [WidgetExamEntry]()
+    var blockPoints: Int = 0
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 8)
+            ForEach(exams) { exam in
+                examView(exam)
+            }
+        }
+    }
+    
+    func examView(_ exam: WidgetExamEntry) -> some View {
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8)
+                Text(exam.name)
+            }
+            Text(String(exam.points))
+        }
+    }
 }
   
