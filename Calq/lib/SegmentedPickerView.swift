7e4e2c8cc353411e260b4aed6e98408bfbe56e45
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2023-12-08 19:43:19 +0100

    Add custom picker View

--- Calq/lib/SegmentedPickerView.swift
+++ Calq/lib/SegmentedPickerView.swift
@@ -9,34 +9,44 @@ import SwiftUI
 
 struct SegmentedPickerView: View {
     @State var index = 0
-    @State var entries: [String] = ["1", "2", "3"]
+    @State var entries: [String] = ["1", "2", "3", "4"]
     @ObservedObject var vm: SegmentedPickerViewModel
     
     var body: some View {
-        HStack {
-            ForEach(Array(entries.enumerated()), id: \.offset) { index, item in
-                
-                Button {
-                    vm.changedIndex(index)
-                } label: {
-                    VStack {
-                        Text(item)
-                            .foregroundColor(vm.color)
-                        if vm.slectedIndex == index {
-                            Rectangle()
-                                .fill(vm.color)
-                                .frame(height: 2)
-                                .padding(.horizontal)
-                            
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.white)
+                .frame(height: 45)
+                .shadow(radius: 2).padding()
+            
+            HStack {
+                ForEach(Array(entries.enumerated()), id: \.offset) { index, item in
+                    let isSelected = vm.selectedIndex == index
+                    Button {
+                        vm.changedIndex(index)
+                    } label: {
+                        VStack (spacing: 5) {
+                            Text(item)
+                                .foregroundColor(isSelected ? vm.color : .gray)
+                            if isSelected {
+                                Rectangle()
+                                    .fill(vm.color)
+                                    .frame(height: 2)
+                                    .padding(.horizontal)
+                                
+                            }
                         }
+                    }.frame(maxWidth: .infinity)
+                    
+                    if index <= entries.count - 2 {
+                        Divider()
                     }
-                }.frame(maxWidth: .infinity)
-                
-                if index <= entries.count - 1 {
-                    Divider()
                 }
             }
-        }.frame(height: 30)
+            .frame(height: 30)
+            .padding()
+            
+        }
     }
 }
 
@@ -48,11 +58,12 @@ struct SegmentedPickerView_Preview: PreviewProvider {
 
 class SegmentedPickerViewModel: ObservableObject {
     @Published var color: Color = .red
-    @Published var slectedIndex: Int = 0
+    @Published var selectedIndex: Int = 0
     
     func changedIndex(_ index: Int) {
+        if selectedIndex == index { return }
         withAnimation {
-            slectedIndex = index
+            selectedIndex = index
         }
     }
 }
