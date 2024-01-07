7e8d2917db81ebeafbb7ebc8d1304898b34fb48f
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2024-01-07 12:24:17 +0100

    Add new model

--- Calq/lib/JSON/Models.swift
+++ Calq/lib/JSON/Models.swift
@@ -7,8 +7,45 @@
 
 import Foundation
 
-// Struct for importing from json
-struct AppStructV2: Codable {
+struct AppStructV3: Codable { // Model 4
+    var colorfulCharts: Bool
+    var hasFiveExams: Bool
+    var showGradeTypes: Bool
+    var usersubjects: [SubjectStruct]
+    var gradeTypes: [JSONTypes]
+    var highlightedType: Int
+    var formatVersion: Int
+    
+    struct JSONTypes: Codable {
+        var name: String
+        var weigth: Int
+        var id: Int
+    }
+    
+    struct SubjectStruct: Codable {
+        var name: String
+        var lk: Bool
+        var color: String
+        var inactiveYears: String
+        var lastEditedAt: Date
+        var createdAt: Date
+        var subjecttests: [JSONTest]
+        
+        struct JSONTest: Codable {
+            var name: String
+            var year: Int
+            var grade: Int
+            var date: String
+            var type: Int
+            var isWrittenGrade: Bool
+            var lastEditedAt: Date
+            var createdAt: Date
+        }
+    }
+}
+
+
+struct AppStructV2: Codable { // Model 3
     var colorfulCharts: Bool
     var hasFiveExams: Bool
     var usersubjects: [SubjectStruct]
