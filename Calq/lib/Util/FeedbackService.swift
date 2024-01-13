15d13bca73d984dd0f6bb1a7837cb9cf53920122
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2024-01-13 16:07:57 +0100

    Edit feedback to embed

--- Calq/lib/Util/FeedbackService.swift
+++ Calq/lib/Util/FeedbackService.swift
@@ -15,15 +15,36 @@ class FeedbackService {
     
     static func sendFeedback(_ content: String) -> Bool {
         guard let url = URL(string: "https://discord.com/api/webhooks/1195726763649671168/XQATWsv9xGYVdamGsZDuc6kPX3hGyu5v5iC_wWssqErq2riswmA3vriFKMbiOcq6Pvxs") else { return false }
-        let message = "**\(app)**\n*\(version)*\n*\(device)*\n-----------------------\n\(content)"
-        let messageJSON: [String: Any] = ["content": message]
-        let jsonData = try? JSONSerialization.data(withJSONObject: messageJSON)
+        let message = "\(content)\n-----------------------\n**\(version)**\n*\(device)*"
+        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "content-type")
-        request.httpBody = jsonData
+        request.httpBody = makeEmbed(message)
+        
         let task = URLSession.shared.dataTask(with: request)
         task.resume()
         return true
+    }
+    
+    private static func makeEmbed(_ content: String) -> Data {
+        let feedback = Feedback(embeds: [Feedback.Embed(title: "\(app) Feedback", color: "7194610", description: content)])
+        
+        do {
+            let data = try JSONEncoder().encode(feedback)
+            return data
+        } catch {
+            return Data()
+        }
+    }
+}
+
+struct Feedback: Codable {
+    var embeds: [Embed]
+    
+    struct Embed: Codable {
+        var title: String
+        var color: String
+        var description: String
     }
 }
